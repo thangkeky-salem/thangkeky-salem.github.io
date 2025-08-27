436a4c57d519ae4c74323acdf650bd3a9988dc10
thangkeky-salem <thangkeky@gmail.com>
2025-08-27 14:43:49 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
-// Cuộn mượt khi bấm menu
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
 
+$(document).ready(function() {
+    // nav toggle
+    $(".hamburger-btn").click(function(){
+        $(".header .nav").slideToggle();
+    })
+    $(".header .nav a").click(function(){
+        if($(window).width()<768){
+            $(".header .nav").slideToggle();
+        }
+    })
+    // audio
+    $("#myAudio")[0].play();
+    $(".bi-music-note-beamed").click(function(){
+        if($(this).hasClass("pause")){
+            $("#myAudio")[0].play();
+        }else{
+        $("#myAudio")[0].pause();
+        }
+    $(this).toggleClass("pause");
+    })
+    //fixed header
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $(".header").addClass("fixed");
+        }
+        else{
+            $(".header").removeClass("fixed");
+        }
+    })
+    //scroll
+        $.scrollIt({
+            topOffset: -50
+        });
+    
+});
+    
